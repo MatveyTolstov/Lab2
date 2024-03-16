73c6dd1edb1ccd80dbb985e84994e375abf566c0
MatveyTolstov <144270275+MatveyTolstov@users.noreply.github.com>
2024-03-16 22:46:55 +0300

    Lab2

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lab1Kokoro.NIGHTCLUBDataSetTableAdapters;
+
+namespace Lab1Kokoro
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Clients_Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClientsWindow clientsWindow = new ClientsWindow();
+            clientsWindow.Show();
+            Close();
+        }
+
+        private void Events_Buttons_Click(object sender, RoutedEventArgs e)
+        {
+            EventWindow eventsWindow = new EventWindow();
+            eventsWindow.Show();
+            Close();
+        }
+
+        private void TableBooking_Buttons_Click(object sender, RoutedEventArgs e)
+        {
+            TableBooking table =  new TableBooking();
+            table.Show();
+            Close();
+        }
+    }
+}
